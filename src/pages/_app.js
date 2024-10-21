f1ff2b2ecbc2cfa56f32305f0bf97be6f1b3d447
Rodrigo Caraveo Moreno <123745168+Moreno2825@users.noreply.github.com>
2024-10-20 20:06:01 -0600

    feat(); files for pwa

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <title>Customer Web App</title>
+        <meta name="description" content="Customer PWA" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#fff" />
+        <link rel="apple-touch-icon" href="/icon512_rounded.png" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
